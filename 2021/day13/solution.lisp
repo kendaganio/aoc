98d8f33bcfc3c2d41ccf98aea1ccfb06000e092d
Ken-Lauren Daganio <akosiken@gmail.com>
2021-12-13 14:47:22 +0800

    day 13 tidying up

--- 2021/day13/solution.lisp
+++ 2021/day13/solution.lisp
@@ -1,20 +1,16 @@
 (require :asdf)
 
 (defvar graph nil)
-(defvar points nil)
 (defvar folds nil)
-(defvar max-x nil)
-(defvar max-y nil)
 
-(defun parse-input (filename)
+(defun parse-input (filename &aux (max-x 0) (max-y 0) (points nil))
   (with-open-file (stream filename)
     (loop for line = (read-line stream nil)
           while line do 
-          (progn
-            (if (search "," line)
-              (setf points (cons (uiop:split-string line :separator ",") points)))
-            (if (search "fold" line)
-              (setf folds (cons (uiop:split-string (caddr (uiop:split-string line :separator " ")) :separator "=") folds))))))
+          (if (search "," line)
+            (setf points (cons (uiop:split-string line :separator ",") points)))
+          (if (search "fold" line)
+            (setf folds (cons (uiop:split-string (caddr (uiop:split-string line :separator " ")) :separator "=") folds)))))
   (setf max-y (1+ (apply 'max (loop for p in points collect (parse-integer (car p))))))
   (setf max-x (1+ (apply 'max (loop for p in points collect (parse-integer (cadr p))))))
   (setf graph (make-array (list max-x max-y) :initial-element "."))
@@ -47,25 +43,21 @@
           (setf (aref new-graph j i) "."))))
   (identity new-graph))
 
-(defun solve1 (input &aux (steps 100))
+(defun fold (graph command)
+  (if (equalp (car command) "y")
+    (setf graph (fold-y graph (parse-integer (cadr command))))
+    (setf graph (fold-x graph (parse-integer (cadr command))))))
+
+(defun solve1 (input &aux (first-fold nil))
   (setf first-fold (car (reverse folds)))
-
-  (if (equalp (car first-fold) "y")
-    (setf graph (fold-y graph (parse-integer (cadr first-fold)))))
-  (if (equalp (car first-fold) "x")
-    (setf graph (fold-x graph (parse-integer (cadr first-fold)))))
-
+  (setf graph (fold graph first-fold))
   (destructuring-bind (n m) (array-dimensions graph)
     (loop for i from 0 below n
       sum (loop for j from 0 below m 
             count (equal "#" (aref graph i j))))))
 
 (defun solve2 (input &aux (mega-flash nil))
-  (loop for f in (reverse folds) do
-        (if (equalp (car f) "y")
-          (setf graph (fold-y graph (parse-integer (cadr f)))))
-        (if (equalp (car f) "x")
-          (setf graph (fold-x graph (parse-integer (cadr f))))))
+  (loop for f in (reverse folds) do (setf graph (fold graph f)))
   (destructuring-bind (n m) (array-dimensions graph)
     (loop for i from 0 below n do
       (loop for j from 0 below m do
@@ -73,11 +65,7 @@
       (terpri))))
 
 (time (format t "[PART1]: ~a" (solve1 (parse-input "./in.txt"))))
-
 ; need to reset the vars for part 2
 (setf graph nil)
-(setf points nil)
 (setf folds nil)
-(setf max-x nil)
-(setf max-y nil)
 (time (format t "[PART2]: ~a" (solve2 (parse-input "./in.txt"))))
