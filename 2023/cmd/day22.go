e75d51e495501733e2c6a4dc975165b5eb2274a0
Ken-Lauren Daganio <akosiken@gmail.com>
2023-12-22 23:46:03 +0200

    2023: day 22 tiny refactor

--- 2023/cmd/day22.go
+++ 2023/cmd/day22.go
@@ -19,9 +19,11 @@ type ThreeDPoint struct {
 }
 
 type Brick struct {
-	ID    int
-	Start ThreeDPoint
-	End   ThreeDPoint
+	ID          int
+	Start       ThreeDPoint
+	End         ThreeDPoint
+	Supports    mapset.Set[int]
+	SupportedBy mapset.Set[int]
 }
 
 func NewBrick(raw string, id int) Brick {
@@ -30,17 +32,21 @@ func NewBrick(raw string, id int) Brick {
 	endRaw := magic.SplitToInts(split[1], ",")
 
 	return Brick{
-		ID:    id,
-		Start: ThreeDPoint{startRaw[0], startRaw[1], startRaw[2]},
-		End:   ThreeDPoint{endRaw[0], endRaw[1], endRaw[2]},
+		ID:          id,
+		Start:       ThreeDPoint{startRaw[0], startRaw[1], startRaw[2]},
+		End:         ThreeDPoint{endRaw[0], endRaw[1], endRaw[2]},
+		Supports:    mapset.NewSet[int](),
+		SupportedBy: mapset.NewSet[int](),
 	}
 }
 
 func (b Brick) Drop() Brick {
 	return Brick{
-		ID:    b.ID,
-		Start: ThreeDPoint{b.Start.X, b.Start.Y, b.Start.Z - 1},
-		End:   ThreeDPoint{b.End.X, b.End.Y, b.End.Z - 1},
+		ID:          b.ID,
+		Start:       ThreeDPoint{b.Start.X, b.Start.Y, b.Start.Z - 1},
+		End:         ThreeDPoint{b.End.X, b.End.Y, b.End.Z - 1},
+		Supports:    b.Supports,
+		SupportedBy: b.SupportedBy,
 	}
 }
 
@@ -51,16 +57,6 @@ func (hp *HyperPlane) Insert(b Brick) {
 		for y := b.Start.Y; y <= b.End.Y; y++ {
 			for z := b.Start.Z; z <= b.End.Z; z++ {
 				(*hp)[ThreeDPoint{x, y, z}] = b.ID
-			}
-		}
-	}
-}
-
-func (hp *HyperPlane) Remove(b Brick) {
-	for x := b.Start.X; x <= b.End.X; x++ {
-		for y := b.Start.Y; y <= b.End.Y; y++ {
-			for z := b.Start.Z; z <= b.End.Z; z++ {
-				delete(*hp, ThreeDPoint{x, y, z})
 			}
 		}
 	}
@@ -81,12 +77,11 @@ func (hp HyperPlane) HasCollision(b Brick) (id []int, hit bool) {
 		}
 	}
 
-	// hits the floor
-
 	if len(hits) > 0 {
 		return maps.Keys(hits), true
 	}
 
+	// hits the floor
 	if b.Start.Z <= 1 {
 		return []int{}, true
 	} else {
@@ -94,47 +89,38 @@ func (hp HyperPlane) HasCollision(b Brick) (id []int, hit bool) {
 	}
 }
 
-type Dep struct {
-	Supports    []int
-	SupportedBy []int
-}
+func collapseBricksAbove(bricks map[int]Brick, brick Brick) mapset.Set[int] {
+	falling := mapset.NewSet[int](brick.ID)
+	q := lane.NewMinPriorityQueue[Brick, int]()
+	q.Push(brick, brick.Start.Z)
 
-func findAllSupportedBricks(depGraph map[int]Dep, id int) mapset.Set[int] {
-	falling := mapset.NewSet[int]()
-	falling.Add(id)
-	q := []int{id}
+	for !q.Empty() {
+		b, _, _ := q.Pop()
 
-	for len(q) > 0 {
-		nextID := q[0]
-		q = q[1:]
+		for _, supportedBrickId := range b.Supports.ToSlice() {
+			if s, ok := bricks[supportedBrickId]; ok {
 
-		if deps, ok := depGraph[nextID]; ok {
-			for _, s := range deps.Supports {
-				if supDep, ok := depGraph[s]; ok {
-					supportedBySet := mapset.NewSet[int](supDep.SupportedBy...)
-
-					if supportedBySet.Difference(falling).Cardinality() < 1 {
-						if !falling.Contains(s) {
-							falling.Add(s)
-							q = append(q, s)
-						}
-					}
+				if s.SupportedBy.Difference(falling).Cardinality() < 1 {
+					falling.Add(supportedBrickId)
+					q.Push(s, s.Start.Z)
 				}
 			}
 		}
 	}
 
+	falling.Remove(brick.ID)
 	return falling
 }
 
 func SolveD22P1(lines []string) (total int) {
 	q := lane.NewMinPriorityQueue[Brick, int]()
+	zappable, collapsed := 0, 0
 	hp := make(HyperPlane)
-	dropped := []Brick{}
-	depGraph := map[int]Dep{}
+	bricks := map[int]Brick{}
 
 	for i, l := range lines {
 		b := NewBrick(l, i)
+		bricks[b.ID] = b
 		q.Push(b, b.Start.Z)
 	}
 
@@ -143,53 +129,36 @@ func SolveD22P1(lines []string) (total int) {
 
 		for {
 			if hits, collided := hp.HasCollision(b.Drop()); collided {
-				if _, ok := depGraph[b.ID]; !ok {
-					depGraph[b.ID] = Dep{[]int{}, []int{}}
-				}
+				for _, hit := range hits {
+					b.SupportedBy.Add(hit)
 
-				for _, i := range hits {
-					if _, ok := depGraph[i]; !ok {
-						depGraph[i] = Dep{[]int{}, []int{}}
-					}
-
-					if g, ok := depGraph[b.ID]; ok {
-						g.SupportedBy = append(g.SupportedBy, i)
-						depGraph[b.ID] = g
-					}
-
-					if g, ok := depGraph[i]; ok {
-						g.Supports = append(g.Supports, b.ID)
-						depGraph[i] = g
+					if below, ok := bricks[hit]; ok {
+						below.Supports.Add(b.ID)
 					}
 				}
-
 				break
 			}
-
 			b = b.Drop()
 		}
 
 		hp.Insert(b)
-		dropped = append(dropped, b)
 	}
 
-	zappable := 0
-	supported := 0
-	for brickID := 0; brickID < len(dropped); brickID++ {
-		if deps, ok := depGraph[brickID]; ok {
-			for _, supportedBrick := range deps.Supports {
-				if len(depGraph[supportedBrick].SupportedBy)-1 == 0 {
+	for _, brick := range maps.Values(bricks) {
+		for _, supportedBrickID := range brick.Supports.ToSlice() {
+			if supportedBrick, ok := bricks[supportedBrickID]; ok {
+				if supportedBrick.SupportedBy.Cardinality()-1 < 1 {
 					zappable++
-					supported += findAllSupportedBricks(depGraph, brickID).Cardinality() - 1
+					collapsed += collapseBricksAbove(bricks, brick).Cardinality()
 					break
 				}
 			}
 		}
-
 	}
 
-	fmt.Println("Part 2:", supported)
-	return len(dropped) - zappable
+	fmt.Println("Part 1:", len(bricks)-zappable)
+	fmt.Println("Part 2:", collapsed)
+	return
 }
 
 var day22Cmd = &cobra.Command{
